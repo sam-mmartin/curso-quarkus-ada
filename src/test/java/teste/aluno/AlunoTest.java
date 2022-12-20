b451507c19533f0eced4f5688ef82176691567ff
sam-mmartin <smeirelesmartins@gmail.com>
2022-12-20 18:08:47 +0000

    implementado testes para classe aluno

--- src/test/java/teste/aluno/AlunoTest.java
+++ src/test/java/teste/aluno/AlunoTest.java
@@ -18,7 +18,7 @@ public class AlunoTest {
    @Test
    @Order(1)
    public void testPostRequest() {
-      String requestBody = "{\"nome\": \"Juliana Minha Gata\", \"cpf\": \"001.002.003-04\"}";
+      String requestBody = "{\"nome\": \"Juliana Cintra\", \"cpf\": \"001.002.003-04\"}";
 
       Response response = given()
             .header("Content-type", "application/json")
@@ -39,5 +39,53 @@ public class AlunoTest {
    @Order(3)
    public void testGetRequestById() {
       given().when().get("/alunos/1").then().statusCode(200);
+   }
+
+   @Test
+   @Order(4)
+   public void testPutRequest() {
+
+      Response responseGET = given().when().get("/alunos/1").then().extract().response();
+      String matricula = responseGET.jsonPath().getString("matricula");
+      String requestBody = "{\"nome\": \"Juliana Sintra\", \"cpf\": \"001.002.003-04\"}";
+
+      Response response = given()
+            .header("Content-type", "application/json")
+            .and()
+            .body(requestBody).when()
+            .put("/alunos/atualizar-cadastro/" + matricula)
+            .then().extract().response();
+
+      Assertions.assertTrue(response.statusCode() == 200);
+      Assertions.assertEquals("Cadastro atualizado com sucesso.", response.jsonPath().getString("texto"));
+   }
+
+   @Test
+   @Order(5)
+   public void testGetByMatricula() {
+
+      Response responseGET = given().when().get("/alunos/1").then().extract().response();
+      String matricula = responseGET.jsonPath().getString("matricula");
+
+      given().when().get("/alunos/matricula/" + matricula).then().statusCode(200);
+   }
+
+   @Test
+   @Order(6)
+   public void testCancelarMatricula() {
+      Response responseGET = given().when().get("/alunos/1").then().extract().response();
+      String matricula = responseGET.jsonPath().getString("matricula");
+
+      given().when().put("/alunos/cancelar-matricula/" + matricula).then().statusCode(200);
+   }
+
+   @Test
+   @Order(7)
+   public void testRematriculaAluno() {
+
+      Response responseGET = given().when().get("/alunos/1").then().extract().response();
+      String matricula = responseGET.jsonPath().getString("matricula");
+
+      given().when().put("/alunos/rematricular/" + matricula).then().statusCode(200);
    }
 }
